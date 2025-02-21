f5360588da4bb1b00745d7d40b04782d8260b1fa
Radomir Djokovic <161573876+RadomirDjokovic4492@users.noreply.github.com>
2025-02-21 08:48:35 +0100

    wip: :construction: add types to LatestUpdates component

--- src/components/LatestUpdates.vue
+++ src/components/LatestUpdates.vue
@@ -1,40 +1,56 @@
 <script setup lang="ts">
-//
+enum ECategory {
+  NEWS = 'news',
+  ESSAY = 'essay',
+}
+
+interface ICategory {
+  value: string
+  title: string
+}
+
+interface Article {
+  title: string
+  publishDate: string
+  category: ECategory
+  url: string
+}
+
 window.LATEST_ARTICLES = [
   {
     title: 'Find new ways to travel north',
     publishDate: '2023-06-03T13:51:50.417Z',
-    category: 'news',
+    category: ECategory.NEWS,
     url: '/articles/4738.html',
   },
   {
     title: 'When will it become possible to use time travel in order to fix your earlier mistakes?',
     publishDate: '2023-06-01T12:18:10.317Z',
-    category: 'essay',
+    category: ECategory.ESSAY,
     url: '/articles/7256.html',
   },
   {
     title: '10 ways to write better text',
     publishDate: '2023-06-03T09:00:32.200Z',
-    category: 'news',
+    category: ECategory.NEWS,
     url: '/articles/7247.html',
   },
   {
     title: 'Announcement: we have a new website category',
     publishDate: '2023-05-30T17:12:13.102Z',
-    category: 'news',
+    category: ECategory.NEWS,
     url: '/articles/1749.html',
   },
   {
     title: 'Weekly news',
     publishDate: '2023-05-29T00:23:15.276Z',
-    category: 'news',
+    category: ECategory.NEWS,
     url: '/articles/1538.html',
   },
   {
     title: 'In-depth travel guide for Tanzania',
     publishDate: '2023-05-31T11:12:43.003Z',
-    category: 'essay',
+    category: ECategory.ESSAY,
     url: '/articles/2594.html',
   },
 ]
@@ -42,12 +58,23 @@ window.LATEST_ARTICLES = [
 import { ref, onMounted } from 'vue'
 
 const data = ref<{
-  latestUpdates: []
+  latestUpdates: Article[]
+  categories: ICategory[]
 } | null>(null)
 
 onMounted(() => {
   data.value = {
     latestUpdates: window.LATEST_ARTICLES,
+    categories: [
+      {
+        value: ECategory.NEWS,
+        title: 'News',
+      },
+      {
+        value: ECategory.ESSAY,
+        title: 'Essay',
+      },
+    ],
   }
 })
 </script>
